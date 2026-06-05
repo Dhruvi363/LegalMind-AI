b407d92ed83d592d9b41aee215e3a32e7c55d472
Dhruvi Mali <dhruvimali363@gmail.com>
2026-06-05 20:23:45 +0530

    fix deployment and python 3.11

--- app/nodes/ipc_retriever.py
+++ app/nodes/ipc_retriever.py
@@ -2,22 +2,24 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
 _db = None
+_embeddings = None
 
 def get_db():
-    global _db
+    global _db, _embeddings
 
     if _db is None:
-        embeddings = HuggingFaceEmbeddings(
-            model_name="sentence-transformers/all-MiniLM-L6-v2"
-        )
+
+        if _embeddings is None:
+            _embeddings = HuggingFaceEmbeddings(
+                model_name="sentence-transformers/all-MiniLM-L6-v2"
+            )
 
         _db = Chroma(
             persist_directory="app/rag/vectordb",
-            embedding_function=embeddings
+            embedding_function=_embeddings
         )
 
     return _db
-
 
 def ipc_retriever(state):
     query = state["user_query"]
