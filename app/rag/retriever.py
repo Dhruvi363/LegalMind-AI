69e9b2b957ca82e586434e057c92f652dcece37f
Dhruvi Mali <dhruvimali363@gmail.com>
2026-06-05 16:52:41 +0530

    change

--- app/rag/retriever.py
+++ app/rag/retriever.py
@@ -1,9 +1,8 @@
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
-embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
+#embeddings = HuggingFaceEmbeddings(
+#    model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 db = Chroma(
     persist_directory="app/rag/vectordb",
